fa12a51c257efcc8dcff4bab416dbc27617e3112
ericadam <ericadam@LI004-43L023153.ad.weber.edu>
2025-09-02 17:09:02 -0600

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/* Final Project for CS3250
+ * 
+ * Fall 2025
+ * 
+ * Eric Adam
+ * 
+ * */
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to my project");
+	}
+
+}
